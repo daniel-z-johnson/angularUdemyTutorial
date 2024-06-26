9ef8eb4833c3ba4160d4b3e49681ae9cf5a50642
daniel-z-johnson <danzaljohnson@gmail.com>
2024-06-26 01:27:49 -0500

    Completed all but the last item in section 22

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import {Recipe} from "../recipe.model";
 
 @Component({
@@ -7,7 +7,15 @@ import {Recipe} from "../recipe.model";
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe("A Test Recipe", "Simple Test", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Meatfoodgroup.jpg/330px-Meatfoodgroup.jpg")
+    new Recipe("A Test Recipe", "Simple Test", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Meatfoodgroup.jpg/330px-Meatfoodgroup.jpg"),
+    new Recipe("A Test Recipe 2", "Simple Test 2", "https://upload.wikimedia.org/wikipedia/commons/d/d8/Channelcat.jpg"),
+    new Recipe("A Test Recipe 3", "Simple Test 3", "https://upload.wikimedia.org/wikipedia/commons/5/56/Tondo_Minotaur_London_E4_MAN.jpg")
   ];
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
